1e9c28c5bfe9add898de9a306dee76201781d397
Paul Sinnett <paul.sinnett@gmail.com>
2020-11-02 15:18:56 +0000

    Initial commit

---
+++ Assets/TriggerLight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerLight : MonoBehaviour
+{
+    public Light spotLight;
+    public Color normalColour;
+    public Color triggerColour;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        spotLight.color = triggerColour;
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        spotLight.color = normalColour;
+    }
+}
